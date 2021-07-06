805bad9c71a38bc4a880e6c963f94648b6d94ad8
ver2point0 <jmci10@hotmail.com>
2021-07-06 15:27:53 -0700

    add videos

--- modules/videos.py
+++ modules/videos.py
@@ -1,19 +1,6 @@
-import csv
-import os
-
-from modules.inventory import Inventory
-from modules.customer import Customers
-
-class Videos(Inventory):
-    def __init__(self, id, title, rating, copies_available):
-        super().__init__(id, title, rating, copies_available)
-        self.checked_out = 'y'
-
-    def __str__(self):
-        return f"""
-        Video ID: {self.id}
-        Title:  {self.title}
-        Rating: {self.rating}
-        Copies Avaliable: {self.copies_available}
-        Checked out: {self.checked_out}
-        """
+class Video():
+    def __init__(self, video_id, title, rating):
+        self.id = video_id
+        self.title = title
+        self.rating = rating
+        self.copies_available = 1
